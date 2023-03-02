e034bff1f95b724781f373f1314d66171f5b543e
nishinotakaya <takaya314boxing@gmail.com>
2023-03-02 21:12:59 +0900

    add/contribとtodoのサーバー側の処理の追加

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'tiny_tds'
 gem 'activerecord-sqlserver-adapter', '~> 3.1.0'
+gem 'sinatra-contrib', '~> 3.0', '>= 3.0.5'
--- my_app.rb
+++ my_app.rb
@@ -1,10 +1,39 @@
 require 'sinatra'
+require 'json'
+require 'sinatra/contrib'
+
+set :allow_origin, "*"
+set :allow_methods, "GET,HEAD,POST,PUT,DELETE,OPTIONS"
+set :allow_headers, "content-type,if-modified-since"
+set :expose_headers, "location,link"
 
 before do
-  response.headers['Access-Control-Allow-Origin'] = '*'
+  content_type :json    
 end
 
-get '/' do
-  content_type :json
-  { key1: 'value1', key2: 'value2' }.to_json
+todos = []
+
+get '/todos' do
+  todos.to_json
+end
+
+post '/todos' do
+  request.body.rewind
+  data = JSON.parse request.body.read
+  todos << { id: todos.length + 1, task: data['task'], isCompleted: false }
+  { message: 'Todo created successfully.' }.to_json
+end
+
+put '/todos/:id' do |id|
+  todo = todos.find { |t| t[:id] == id.to_i }
+  request.body.rewind
+  data = JSON.parse request.body.read
+  todo[:task] = data['task'] if data['task']
+  todo[:isCompleted] = data['isCompleted'] if data['isCompleted']
+  { message: 'Todo updated successfully.' }.to_json
+end
+
+delete '/todos/:id' do |id|
+  todos.reject! { |t| t[:id] == id.to_i }
+  { message: 'Todo deleted successfully.' }.to_json
 end
